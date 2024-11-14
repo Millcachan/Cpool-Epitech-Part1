8ce0658b42dbf9704bcf1359c93cd05e1fd98344
Camille <camille.billard@epitech.eu>
2024-11-14 16:43:05 +0100

    feat: update my_strupcase.c

--- Day_06/my_strupcase.c
+++ Day_06/my_strupcase.c
@@ -7,8 +7,8 @@
 
 char *my_strupcase(char *str)
 {
-    for (int i = 0; str[i] != '\0'; i++)
-        if (str[i] >= 97 && str[i] <= 122)
+    for (int i = 0; str[i]; i++)
+        if (str[i] >= 'a' && str[i] <= 'z')
             str[i] = str[i] - 32;
-    return (str);
+    return str;
 }
