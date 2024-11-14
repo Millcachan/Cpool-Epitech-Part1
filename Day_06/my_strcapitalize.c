0fa58624eef11a0f7dee0239ad06a234aba8cb66
Camille <camille.billard@epitech.eu>
2024-11-14 16:47:25 +0100

    feat: update my_strcapitalize.c

--- Day_06/my_strcapitalize.c
+++ Day_06/my_strcapitalize.c
@@ -9,17 +9,17 @@ char *my_strcapitalize(char *str)
 {
     int new_word = 1;
 
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] < 65 || (str[i] > 90 && str[i] < 97) || str[i] > 122)
+    for (int i = 0; str[i]; i++) {
+        if (str[i] < 'A' || (str[i] > 'Z' && str[i] < 'a') || str[i] > 'z')
             new_word = 1;
-        if (str[i] >= 65 && str[i] <= 90 && new_word == 0)
+        if (str[i] >= 'A' && str[i] <= 'Z' && !new_word)
             str[i] = str[i] + 32;
-        if (str[i] >= 65 && str[i] <= 90 && new_word == 1)
+        if (str[i] >= 'A' && str[i] <= 'Z' && new_word)
             new_word = 0;
-        if ((str[i] >= 97 && str[i] <= 122 && new_word == 1)) {
+        if ((str[i] >= 'a' && str[i] <= 'z' && new_word)) {
             str[i] = str[i] - 32;
             new_word = 0;
         }
     }
-    return (str);
+    return str;
 }
