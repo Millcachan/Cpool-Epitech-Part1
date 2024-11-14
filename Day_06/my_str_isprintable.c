5a17448c8ec922385a81fac28da5d260fe91a7e7
Camille <camille.billard@epitech.eu>
2024-11-14 16:52:51 +0100

    feat: update my_strisprintable.c

--- Day_06/my_str_isprintable.c
+++ Day_06/my_str_isprintable.c
@@ -7,9 +7,8 @@
 
 int my_str_isprintable(char const *str)
 {
-    for (int i = 0; str[i] != '\0'; i++) {
+    for (int i = 0; str[i]; i++)
         if (str[i] < 32 || str[i] > 126)
-            return (0);
-    }
-    return (1);
+            return 0;
+    return 1;
 }
