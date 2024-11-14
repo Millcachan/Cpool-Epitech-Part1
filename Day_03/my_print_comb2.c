383b0ba1ab7828e42c24a44ba99efcd77aa8deda
Camille <camille.billard@epitech.eu>
2024-11-14 14:50:01 +0100

    feat: update my_print_comb2.c

--- Day_03/my_print_comb2.c
+++ Day_03/my_print_comb2.c
@@ -5,35 +5,22 @@
 ** printcomb2
 */
 
-void print_numbers(int a, int b)
-{
-    my_putchar(a / 10 + 48);
-    my_putchar(a % 10 + 48);
-    my_putchar(' ');
-    my_putchar(b / 10 + 48);
-    my_putchar(b % 10 + 48);
-    if (a != 98) {
-        my_putchar(44);
-        my_putchar(32);
-    }
-}
-
-int my_print2(int a, int b)
-{
-    b = a + 1;
-    while (b <= 99) {
-        print_numbers(a, b);
-        b++;
-    }
-}
-
 int my_print_comb2(void)
 {
     int a = 0;
     int b = 0;
 
     while (a <= 98) {
-        my_print2(a, b);
-        a++;
+        my_putchar(a / 10 + 48);
+        my_putchar(a % 10 + 48);
+        my_putchar(' ');
+        my_putchar(b / 10 + 48);
+        my_putchar(b % 10 + 48);
+        if (a != 98) {
+            my_putchar(',');
+            my_putchar(' ');
+        }
+        a = (b == 99) ? a + 1: a;
+        b = (b < 99) ? b + 1: a + 1;
     }
 }
