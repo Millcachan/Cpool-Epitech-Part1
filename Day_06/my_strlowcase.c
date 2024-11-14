5c96c1ddb0c241064181d5bf5a3291223e5f74f4
Camille <camille.billard@epitech.eu>
2024-11-14 16:43:31 +0100

    feat: update my_strlowcase.c

--- Day_06/my_strlowcase.c
+++ Day_06/my_strlowcase.c
@@ -7,8 +7,8 @@
 
 char *my_strlowcase(char *str)
 {
-    for (int i = 0; str[i] != '\0'; i++)
-        if (str[i] >= 65 && str[i] <= 90)
+    for (int i = 0; str[i]; i++)
+        if (str[i] >= 'A' && str[i] <= 'Z')
             str[i] = str[i] + 32;
-    return (str);
+    return str;
 }
