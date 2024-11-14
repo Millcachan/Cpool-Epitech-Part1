cc9d6faa4df975e4e9f1c06ff47173ca30a1af4e
Camille <camille.billard@epitech.eu>
2024-11-14 16:42:02 +0100

    feat: update my_strncmp.c

--- Day_06/my_strncmp.c
+++ Day_06/my_strncmp.c
@@ -7,16 +7,14 @@
 
 int my_strncmp(char const *s1, char const *s2, int n)
 {
-    if (n <= 0)
-        return (0);
     for (int i = 0; n > 0; i++) {
         if (s1[i] > s2[i])
-            return (s1[i] - s2[i]);
+            return s1[i] - s2[i];
         if (s1[i] < s2[i])
-            return (s2[i] - s1[i]);
+            return s2[i] - s1[i];
         if (s1[i] == s2[i] == '\0')
-            return (0);
-        n = n - 1;
+            return 0;
+        n--;
     }
-    return (0);
+    return 0;
 }
