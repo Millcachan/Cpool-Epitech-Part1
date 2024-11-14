15d171cfaeac0ceddbc13bcff467ee9b94a229ae
Camille <camille.billard@epitech.eu>
2024-11-14 15:59:07 +0100

    feat: update my_is_prime.c

--- Day_05/my_is_prime.c
+++ Day_05/my_is_prime.c
@@ -10,9 +10,9 @@ int my_is_prime(int nb)
     int i = 2;
 
     if (nb <= 1)
-        return (0);
+        return 0;
     for (i = 2; (i * i) <= nb; i++)
         if ((nb % i) == 0)
-            return (0);
-    return (1);
+            return 0;
+    return 1;
 }
