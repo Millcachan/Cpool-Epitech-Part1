c41c6853d8ddaf9b46f7f0a9be0b18251a0c3897
Camille <camille.billard@epitech.eu>
2024-11-14 15:47:16 +0100

    feat: update my_compute_factoril_rec.c

--- Day_05/my_compute_factorial_rec.c
+++ Day_05/my_compute_factorial_rec.c
@@ -8,9 +8,8 @@
 int my_compute_factorial_rec(int nb)
 {
     if (nb == 0)
-        return (1);
+        return 1;
     if (nb < 0 || nb > 12)
-        return (0);
-    nb = (nb != 1) ? nb * my_compute_factorial_rec(nb - 1) : nb;
-    return (nb);
+        return 0;
+    return (nb != 1) ? nb * my_compute_factorial_rec(nb - 1) : nb;
 }
