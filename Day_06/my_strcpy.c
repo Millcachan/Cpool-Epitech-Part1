0ee6bbddc484c131e86e66d581221fb36fa5f10f
Camille <camille.billard@epitech.eu>
2024-11-14 16:14:42 +0100

    feat: update my_strcpy.c

--- Day_06/my_strcpy.c
+++ Day_06/my_strcpy.c
@@ -5,11 +5,11 @@
 ** strcpy
 */
 
-char *my_strcpy(char *dest , char const *src)
+char *my_strcpy(char *dest, char const *src)
 {
     int i;
 
-    for (i = 0; src[i] != '\0'; i++)
+    for (i = 0; src[i]; i++)
         dest[i] = src[i];
     dest[i] = '\0';
     return dest;
