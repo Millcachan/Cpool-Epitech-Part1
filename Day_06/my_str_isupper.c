f2ce66348174084b00b4b4310bcd293ce38d4a94
Camille <camille.billard@epitech.eu>
2024-11-14 16:52:18 +0100

    feat: update my_strisupper.c

--- Day_06/my_str_isupper.c
+++ Day_06/my_str_isupper.c
@@ -7,9 +7,8 @@
 
 int my_str_isupper(char const *str)
 {
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] < 65 || str[i] > 90)
-            return (0);
-    }
-    return (1);
+    for (int i = 0; str[i]; i++)
+        if (str[i] < 'A' || str[i] > 'Z')
+            return 0;
+    return 1;
 }
