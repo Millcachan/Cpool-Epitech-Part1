09e897d230713cf11db6ffee93b7938ea2db375b
Camille <camille.billard@epitech.eu>
2024-11-14 16:49:39 +0100

    feat: update my_strislower.c

--- Day_06/my_str_islower.c
+++ Day_06/my_str_islower.c
@@ -7,9 +7,8 @@
 
 int my_str_islower(char const *str)
 {
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] < 97 || str[i] > 122)
-            return (0);
-    }
-    return (1);
+    for (int i = 0; str[i]; i++)
+        if (str[i] < 'a' || str[i] > 'z')
+            return 0;
+    return 1;
 }
