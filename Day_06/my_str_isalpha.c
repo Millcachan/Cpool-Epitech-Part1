426072f2e426921ff6e5746a181aac29b30c7e41
Camille <camille.billard@epitech.eu>
2024-11-14 16:48:49 +0100

    feat: update my_strisalpha.c

--- Day_06/my_str_isalpha.c
+++ Day_06/my_str_isalpha.c
@@ -7,9 +7,8 @@
 
 int my_str_isalpha(char const *str)
 {
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] < 65 || str[i] > 90 && str[i] < 97 || str[i] > 122)
-            return (0);
-    }
-    return (1);
+    for (int i = 0; str[i]; i++)
+        if (str[i] < 'A' || str[i] > 'Z' && str[i] < 'a' || str[i] > 'z')
+            return 0;
+    return 1;
 }
