f2e562f9d15cc266471340ae6e12482847d2ef48
Camille <camille.billard@epitech.eu>
2024-11-15 12:30:59 +0100

    feat: update my_show_word_arra.c

--- Day_08/my_show_word_array.c
+++ Day_08/my_show_word_array.c
@@ -7,9 +7,9 @@
 
 int my_show_word_array(char const **tab)
 {
-    for (int i = 0; tab[i] != 0; i++) {
+    for (int i = 0; tab[i]; i++) {
         my_putstr(tab[i]);
         my_putchar('\n');
     }
-    return (0);
+    return 0;
 }
